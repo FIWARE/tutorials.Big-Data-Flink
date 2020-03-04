a6caa6aa2fdec08e255cf6f1ce3aa203fb474e12
Javierlj <javierlopezjimeno@gmail.com>
2020-03-04 13:02:53 +0100

    ExampleNGSILD

---
+++ job/src/main/scala/org/fiware/cosmos/orion/flink/connector/tutorial/example1/ExampleNGSILD.scala
@@ -0,0 +1,32 @@
+package org.fiware.cosmos.orion.flink.connector.tutorial.example1
+
+import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.fiware.cosmos.orion.flink.connector.NGSILDSource
+
+/**
+  * Example1 Orion Connector
+  * @author @Javierlj
+  */
+object ExampleNGSILD{
+
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    // Create Orion Source. Receive notifications on port 9001
+    val eventStream = env.addSource(new NGSILDSource(9001))
+
+    // Process event stream
+    val processedDataStream = eventStream
+      .flatMap(event => event.entities)
+      .map(entity => new Sensor(entity.`type`,1))
+      .keyBy("device")
+      .timeWindow(Time.seconds(60))
+      .sum(1)
+
+    // print the results with a single thread, rather than in parallel
+    processedDataStream.print().setParallelism(1)
+    env.execute("Socket Window NgsiLDEvent")
+  }
+
+  case class Sensor(device: String, sum: Int)
+}
